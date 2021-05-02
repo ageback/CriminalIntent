e05428aebe78d96d0535ac7c751082243bd9db53
Michael Lu <yeskky@gmail.com>
2021-05-02 21:57:11 +0800

    使用RecyclerView、ViewHolder、Adapter机制，实现一个简单的数据列表界面

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,86 @@
+package com.bignerdranch.android.criminalintent
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "CrimeListFragment"
+
+class CrimeListFragment : Fragment() {
+    private lateinit var crimeRecyclerView: RecyclerView
+    private var adapter: CrimeAdapter? = null
+
+    private val crimeListViewModel: CrimeListViewModel by lazy {
+        ViewModelProvider(this).get(CrimeListViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "陋习总数：${crimeListViewModel.crimes.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+        crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
+        // RecyclerView 本身无法在屏幕上给要显示的列表项安排位置。
+        // 它把这项工作委托给LayoutManager处理。
+        // 因此，如果没有LayoutManager在场，程序就会崩溃。
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        // 将RecyclerView和adapter绑定，更新UI
+        updateUI()
+        return view
+    }
+
+    private fun updateUI(){
+        val crimes=crimeListViewModel.crimes
+        adapter=CrimeAdapter(crimes)
+        crimeRecyclerView.adapter=adapter
+    }
+
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+    }
+
+    /**
+     * Adapter 负责创建必要的ViewHolder，并将ViewHolder绑定至模型层数据。
+     * @param crimes 模型层数据
+     */
+    private inner class CrimeAdapter(var crimes: List<Crime>) :
+        RecyclerView.Adapter<CrimeHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+
+            return CrimeHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime = crimes[position]
+            holder.apply {
+                titleTextView.text = crime.title
+                dateTextView.text = crime.date.toString()
+            }
+        }
+
+        override fun getItemCount() = crimes.size
+
+    }
+
+    companion object {
+        fun newIntent(): CrimeListFragment {
+            return CrimeListFragment()
+        }
+    }
+}
